eec5611496cd9cd7d0d88ca1855880f348786f41
Willem <wpreterre.lamanu@gmail.com>
2021-01-05 09:11:22 +0100

    Localisation bouton JS

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var removePanier = document.getElementsByClassName('all-buttonArticle')
+for (var i = 0; i < removePanier.length; i++) {
+    var buttonRemove = removePanier[i]
+    buttonRemove.addEventListener('click', function(event) {
+        var removeButtonClicked = event.target.parentNode.parentNode.remove()
+        //target pour récuperer l'élément qui a déclanché un événement spécifique 
+        //ParentNode pour récuperer le parent du parent pour supprimer l'article
+    })
+}
+var addPanier = document.getElementsByClassName('buttonPlus')
+for (var i = 0; i < addPanier.length; i++) {
+    var buttonAdd = addPanier[i]
+    buttonAdd.addEventListener('click', function(addFunction)  {
+        console.log(buttonAdd)
+    })
+}
+
+var addPanierDetail = document.getElementsByClassName('panier-button')
+for (var i = 0; i < addPanierDetail.length; i++) {
+    var buttonAddDetail = addPanierDetail[i]
+    buttonAddDetail.addEventListener('click', function(addFunction)  {
+        console.log(buttonAddDetail)
+    })
+}
